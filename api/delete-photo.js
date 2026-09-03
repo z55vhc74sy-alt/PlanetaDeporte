4d76f1b84ef4f9a972f8e5efe969b91629e18a9a
z55vhc74sy-alt <z55vhc74sy@privaterelay.appleid.com>
2026-09-03 16:39:40 -0300

    Update delete-photo.js

--- api/delete-photo.js
+++ api/delete-photo.js
@@ -1,1 +1,32 @@
+import { del } from "@vercel/blob";
 
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    return res.status(405).json({
+      error: "Método no permitido"
+    });
+  }
+
+  try {
+    const { url } = req.body;
+
+    if (!url) {
+      return res.status(400).json({
+        error: "Falta la URL de la foto"
+      });
+    }
+
+    await del(url);
+
+    return res.status(200).json({
+      ok: true
+    });
+
+  } catch (error) {
+    console.error("ERROR DELETE PHOTO:", error);
+
+    return res.status(500).json({
+      error: error.message
+    });
+  }
+}
